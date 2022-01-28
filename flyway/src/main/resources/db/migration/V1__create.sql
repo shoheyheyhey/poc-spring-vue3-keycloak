d2535bd96cb4e8478431829c9b3441b9fb647fd9
shohey <shohey.asada0525@gmail.com>
2022-01-29 06:10:00 +0900

    テストデータ登録クエリ追加

--- flyway/src/main/resources/db/migration/V1__create.sql
+++ flyway/src/main/resources/db/migration/V1__create.sql
@@ -1,12 +1,31 @@
-/* SCHEMAを作成 */
+--SCHEMAを作成
 CREATE SCHEMA sample;
 
-/* TABLEを作成 */
-CREATE TABLE sample.user_detail (
-  user_id integer,
-  user_name varchar(255),
-  PRIMARY KEY(user_id)
+--TABLEを作成
+--加盟店企業
+CREATE TABLE sample.company (
+  company_code varchar(255),
+  company_name varchar(255),
+  contract_start_date date,
+  contract_end_date date,
+  PRIMARY KEY(company_code)
 );
-COMMENT ON TABLE sample.user_detail IS 'ユーザ';
-COMMENT ON COLUMN sample.user_detail.user_id IS 'ユーザID';
-COMMENT ON COLUMN sample.user_detail.user_name IS 'ユーザ名';
+COMMENT ON TABLE sample.company IS '加盟企業';
+COMMENT ON COLUMN sample.company.company_code IS '加盟企業コード';
+COMMENT ON COLUMN sample.company.company_name IS '加盟企業名';
+COMMENT ON COLUMN sample.company.contract_start_date IS '契約開始日';
+COMMENT ON COLUMN sample.company.contract_end_date IS '契約終了日';
+
+--店舗
+CREATE TABLE sample.shop (
+  company_code varchar(255),
+  shop_code varchar(255),
+  shop_name varchar(255),
+  address varchar(255),
+  PRIMARY KEY(company_code,shop_code),
+  FOREIGN KEY(company_code) REFERENCES sample.company(company_code)
+);
+COMMENT ON TABLE sample.shop IS '店舗';
+COMMENT ON COLUMN sample.shop.company_code IS '加盟企業コード';
+COMMENT ON COLUMN sample.shop.shop_code IS '店舗コード';
+COMMENT ON COLUMN sample.shop.shop_name IS '店舗名';
